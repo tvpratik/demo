66c96e1dc5aa823031f85d1b63dda9dcc97d1435
tvpratik <pratik@techvoot.com>
2023-05-13 18:03:12 +0530

    current location marker set using google map

--- src/Navigators.tsx
+++ src/Navigators.tsx
@@ -31,7 +31,7 @@ function Navigators(): JSX.Element {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="TodoList"
+        initialRouteName="GoogleMap"
         screenOptions={{
           headerShown: false,
           gestureEnabled: true,
--- src/Screens/GoogleMap.tsx
+++ src/Screens/GoogleMap.tsx
@@ -1,49 +1,142 @@
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable prettier/prettier */
 import React, {useState, useEffect} from 'react';
-import {StyleSheet} from 'react-native';
+import {
+  StyleSheet,
+  PermissionsAndroid,
+  Platform,
+  View,
+  Button,
+} from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 export default function GoogleMap() {
-  const [position, setPosition] = useState({
-    latitude: 37.78825,
-    longitude: -122.4324,
-    latitudeDelta: 0.015,
-    longitudeDelta: 0.0121,
-  });
+  const [currentLongitude, setCurrentLongitude] = useState(22.2855168);
+  const [currentLatitude, setCurrentLatitude] = useState(70.7723264);
+  const [locationStatus, setLocationStatus] = useState(String);
 
   useEffect(() => {
-    Geolocation.getCurrentPosition(pos => {
-      console.log(pos);
-      const crd = pos.coords;
-      setPosition({
-        latitude: parseFloat(crd.latitude),
-        longitude: parseFloat(crd.longitude),
-        latitudeDelta: 0.015,
-        longitudeDelta: 0.0121,
-      });
-    }),
-      error => this.setState({error: error.message}),
-      {enableHighAccuracy: false, timeout: 200000, maximumAge: 1000};
+    const requestLocationPermission = async () => {
+      if (Platform.OS === 'ios') {
+        getOneTimeLocation();
+        subscribeLocationLocation();
+      } else {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          );
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            //To Check, If Permission is granted
+            getOneTimeLocation();
+            subscribeLocationLocation();
+          } else {
+            setLocationStatus('Permission Denied');
+          }
+        } catch (err) {
+          console.warn(err);
+        }
+      }
+    };
+    requestLocationPermission();
+    return () => {
+      Geolocation.clearWatch(watchID);
+    };
   }, []);
+  const getOneTimeLocation = () => {
+    setLocationStatus('Getting Location ...');
+    Geolocation.getCurrentPosition(
+      //Will give you the current location
+      position => {
+        setLocationStatus('You are Here');
 
+        //getting the Longitude from the location json
+        const currentLongitude = parseFloat(position.coords.longitude);
+
+        //getting the Latitude from the location json
+        const currentLatitude = parseFloat(position.coords.latitude);
+
+        //Setting Longitude state
+        setCurrentLongitude(currentLongitude);
+
+        //Setting Longitude state
+        setCurrentLatitude(currentLatitude);
+      },
+      error => {
+        setLocationStatus(error.message);
+      },
+      {
+        enableHighAccuracy: false,
+        timeout: 30000,
+        maximumAge: 1000,
+      },
+    );
+  };
+
+  const subscribeLocationLocation = () => {
+    watchID = Geolocation.watchPosition(
+      position => {
+        //Will give you the location on location change
+
+        setLocationStatus('You are Here');
+        console.log(position);
+
+        //getting the Longitude from the location json
+        const currentLongitude = parseFloat(position.coords.longitude);
+
+        //getting the Latitude from the location json
+        const currentLatitude = parseFloat(position.coords.latitude);
+
+        //Setting Longitude state
+        setCurrentLongitude(currentLongitude);
+
+        //Setting Latitude state
+        setCurrentLatitude(currentLatitude);
+      },
+      error => {
+        setLocationStatus(error.message);
+      },
+      {
+        enableHighAccuracy: false,
+        maximumAge: 1000,
+      },
+    );
+  };
   return (
-    <MapView
-      style={styles.map}
-      //specify our coordinates.
-      initialRegion={position}
-      zoomEnabled={true}
-      showsUserLocation={true}
-      zoomControlEnabled={true}>
-      <Marker
-        coordinate={{
-          latitude: position.latitude,
-          longitude: position.longitude,
+    <View style={{flex: 1}}>
+      <MapView
+        style={styles.map}
+        //specify our coordinates.
+        initialRegion={{
+          latitude: currentLatitude ?? 22.2855168,
+          longitude: currentLongitude ?? 70.7723264,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
         }}
-        title={'Address'}
-        description={'test'}
-      />
-    </MapView>
+        zoomEnabled={true}
+        showsUserLocation={true}
+        zoomControlEnabled={true}
+        loadingEnabled={true}
+        loadingIndicatorColor="#666666"
+        loadingBackgroundColor="#eeeeee"
+        moveOnMarkerPress={false}
+        showsCompass={true}
+        showsPointsOfInterest={false}
+        provider="google">
+        {locationStatus === 'You are Here' ? (
+          <Marker
+            coordinate={{
+              latitude: currentLatitude ?? 22.2855168,
+              longitude: currentLongitude ?? 70.7723264,
+            }}
+            title={'Address'}
+            description={'test'}
+          />
+        ) : null}
+      </MapView>
+      <View style={{bottom: 0, position: 'absolute'}}>
+        <Button title={locationStatus} onPress={getOneTimeLocation} />
+      </View>
+    </View>
   );
 }
 //create our styling code:
